e591e882723b0a0e1b710c14f8c5499809669344
Gavin Sheppard <gavinclivesheppard@gmail.com>
2023-09-03 00:27:00 +1200

    initial commit

---
+++ hello.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+# Flask instance
+app = Flask(__name__)
+
+# create a route decorator
+@app.route('/')
+def index():
+    favorites = ['dfjf', 'kwjhf', 'hffljkf', 'hbflkh']
+    return render_template("index.html", favorites=favorites)
+
+# localhost:5000/user/Gavin
+@app.route('/user/<name>')
+def user(name):
+    return render_template("user.html", name=name)
+
+# error pages
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
