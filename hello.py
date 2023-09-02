6b38cde21541e3003f7e84813a3d8cda0c19dfea
Gavin Sheppard <gavinclivesheppard@gmail.com>
2023-09-03 01:38:05 +1200

    form and bootstrap added

--- hello.py
+++ hello.py
@@ -1,7 +1,18 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # Flask instance
 app = Flask(__name__)
+
+# create a secret key
+app.config['SECRET_KEY'] = "super_key"
+
+# create a form class
+class NamerForm(FlaskForm):
+    name = StringField("Enter name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 # create a route decorator
 @app.route('/')
@@ -23,3 +34,17 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500
+
+# name page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    if form.validate_on_submit():
+        name = form.name.data   # get name from form
+        form.name.data = ''     # reset form
+
+    return render_template("name.html",
+        name = name,
+        form = form,
+        )
